c944c426a8efa4cf50e3c7bd5f58dd5624657d26
Aryan Anand <aryanaa2005@gmail.com>
2026-05-15 12:54:26 +0530

    feat: initialize application router and define site navigation paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,12 @@ import Signup from "./pages/auth/Signup";
 import ErrorPage from "./pages/ErrorPage";
 import NewListing from "./pages/listings/NewListing";
 import MyListings from "./pages/listings/MyListings";
+import MyBookings from "./pages/bookings/MyBookings";
+import BookingDetail from "./pages/bookings/BookingDetail";
 import EditListing from "./pages/listings/EditListing";
 import ListingsIndex from "./pages/listings/ListingsIndex";
 import DisplayListing from "./pages/listings/DisplayListing";
+import BookingConfirm from "./pages/bookings/BookingConfirm";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
@@ -30,8 +33,13 @@ const router = createBrowserRouter([
         path: "/listings/:id/edit",
         element: <EditListing />,
       },
+      {
+        path: "/listings/:id/book",
+        element: <BookingConfirm />,
+      },
       { path: "/mylistings", element: <MyListings /> },
-      { path: "/mybookings", element: <ListingsIndex /> },
+      { path: "/mybookings", element: <MyBookings /> },
+      { path: "/mybookings/:bookingId", element: <BookingDetail /> },
       { path: "*", element: <ErrorPage /> },
     ],
   },
